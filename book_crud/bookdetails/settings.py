5d158db80b1fc21d89ba76f70e2128d892dc139b
Sathvika-g-29 <24b01a1242@svecw.edu.in>
2026-05-23 16:52:49 +0530

    Organize book CRUD project

--- book_crud/bookdetails/settings.py
+++ book_crud/bookdetails/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qfwgq(61x+4bo7%s=4-wr-i%()6g#13rsqvc%z)60thl+0$5ar'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-dev-only-change-me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
